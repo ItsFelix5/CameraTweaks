60876305f5b8f506a7cf71a4de230685acff5025
itsfelix5 <felixsoetewey@outlook.com>
2025-02-22 15:08:35 +0100

    :(

--- src/main/java/cameratweaks/mixin/LightmapTextureManagerMixin.java
+++ src/main/java/cameratweaks/mixin/LightmapTextureManagerMixin.java
@@ -3,6 +3,7 @@ package cameratweaks.mixin;
 import cameratweaks.Config;
 import net.minecraft.client.render.LightmapTextureManager;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -10,6 +11,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LightmapTextureManager.class)
 public class LightmapTextureManagerMixin {
+    @Unique
+    private static boolean disableLightmap = false;
+
     @Redirect(method = "update", at = @At(value = "INVOKE", target = "Ljava/lang/Double;floatValue()F", ordinal = 1))
     private float updateGamma(Double instance) {
         if (Config.HANDLER.instance().fullbright) return 1250;
@@ -18,6 +22,9 @@ public class LightmapTextureManagerMixin {
 
     @Inject(method = "update", at = @At("HEAD"), cancellable = true)
     private void update(CallbackInfo ci) {
-        if (Config.HANDLER.instance().fullbright) ci.cancel();
+        if (Config.HANDLER.instance().fullbright) {
+            if(disableLightmap) ci.cancel();
+            else disableLightmap = true;
+        } else disableLightmap = false;
     }
 }
