a3ca34d2f4a62c23b5557ef786822c0dd9a036ba
itsfelix5 <felixsoetewey@outlook.com>
2025-06-23 12:41:30 +0200

    Finally fix fog

--- src/main/java/cameratweaks/mixin/FogRendererMixin.java
+++ src/main/java/cameratweaks/mixin/FogRendererMixin.java
@@ -7,12 +7,15 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
+import static cameratweaks.Util.client;
+
 @Mixin(FogRenderer.class)
 public class FogRendererMixin {
     @ModifyVariable(method = "applyFog(Lnet/minecraft/client/render/Camera;IZLnet/minecraft/client/render/RenderTickCounter;FLnet/minecraft/client/world/ClientWorld;)Lorg/joml/Vector4f;", at =
-    @At(value = "INVOKE", target = "Lnet/minecraft/client/render/fog/FogRenderer;applyFog(Ljava/nio/ByteBuffer;ILorg/joml/Vector4f;FFFFFF)V"), order = 999)
+    @At(value = "INVOKE", target = "Lnet/minecraft/client/gl/MappableRingBuffer;getBlocking()Lcom/mojang/blaze3d/buffers/GpuBuffer;"))
     private FogData getFogBuffer(FogData value) {
-        if (Config.HANDLER.instance().disableFog) value.renderDistanceStart = value.renderDistanceEnd = value.environmentalStart = value.environmentalEnd = value.cloudEnd;
+        if (Config.HANDLER.instance().disableFog) value.renderDistanceStart = value.renderDistanceEnd = value.environmentalStart = value.environmentalEnd = value.skyEnd = value.cloudEnd =
+                client.options.getClampedViewDistance() * 32;
         return value;
     }
 }
